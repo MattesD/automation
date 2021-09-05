e83ea857d8642896962265e03d07037c0862328d
Mattes <drechsel.mattes@gmail.com>
2021-09-05 23:00:38 +0200

    first commit

---
+++ src/main/java/de/mattes/uol/automation/config/MyPluginJavaConfig.java
@@ -0,0 +1,20 @@
+package de.mattes.uol.automation.config;
+
+import com.atlassian.templaterenderer.TemplateRenderer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+import static com.atlassian.plugins.osgi.javaconfig.OsgiServices.importOsgiService;
+
+@Configuration
+public class MyPluginJavaConfig {
+
+    // imports ApplicationProperties from OSGi
+
+    @Bean
+    public TemplateRenderer templateRenderer() {
+        return importOsgiService(TemplateRenderer.class);
+    }
+}
---
+++ src/main/java/de/mattes/uol/servlet/AutomationServlet.java
@@ -0,0 +1,35 @@
+package de.mattes.uol.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.atlassian.templaterenderer.TemplateRenderer;
+
+@WebServlet("/automation")
+public class AutomationServlet extends HttpServlet{
+    private static final Logger log = LoggerFactory.getLogger(AutomationServlet.class);
+
+    private final TemplateRenderer templateRenderer;
+
+    public AutomationServlet(TemplateRenderer templateRenderer) {
+        this.templateRenderer = templateRenderer;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        resp.setContentType("text/html");
+        templateRenderer.render("index.vm", resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
