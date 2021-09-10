b4067d658c80879e3db0190cf3609b7b6f284b00
Mattes <drechsel.mattes@gmail.com>
2021-09-10 11:50:21 +0200

    doPost auskommentiert

--- src/main/java/de/mattes/uol/servlet/AutomationServlet.java
+++ src/main/java/de/mattes/uol/servlet/AutomationServlet.java
@@ -9,17 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+
 
 import com.atlassian.templaterenderer.TemplateRenderer;
-//import com.opencsv.CSVReader;
+
 
 @WebServlet("/automation")
 @MultipartConfig
@@ -37,30 +31,4 @@ public class AutomationServlet extends HttpServlet {
         resp.setContentType("text/html");
         templateRenderer.render("index.vm", resp.getWriter());
     }
-
-/*    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Part part = req.getPart("file");
-        InputStream fileContent = part.getInputStream();
-
-        Reader in = new InputStreamReader(fileContent);
-
-        CSVReader csvReader;
-        Iterator<String[]> iterator;
-
-        try {
-            csvReader = new CSVReader(new InputStreamReader(fileContent));
-            iterator = csvReader.iterator();
-
-            String[] row = iterator.next();
-
-            for (int i = 0; i < row.length; i++) {
-                System.out.println(row[i]);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        in.close();
-    }*/
 }
